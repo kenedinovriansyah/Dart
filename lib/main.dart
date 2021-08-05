814b24965a85073fae00459f0c5ab160d0632b79
kalilinux <kalilinux@kalilinuxs-MacBook-Air.local>
2021-08-06 01:46:00 +0700

    style widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,32 @@
+// Author Kenedi Novriansyah
+
 import "package:flutter/material.dart";
 
-void main() {
-  runApp(new App());
-}
+void main() => runApp(new MyApps());
 
-class App extends StatelessWidget {
+class MyApps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("New Text On Navbar"),
+          title: Text(
+            "THIS IS NAVBAR GUYS",
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic,
+                fontSize: 10),
+          ),
         ),
         body: Center(
           child: Container(
-            color: Colors.blueAccent,
-            width: 200,
-            height: 200,
             child: Text(
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+              "Hihihi",
               style: TextStyle(
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center,
+                  color: Colors.indigo,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic),
             ),
           ),
         ),
